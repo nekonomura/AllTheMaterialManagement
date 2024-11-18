ac8f854d9fd59ea47cb173f3f0ae4e3942af50d6
nekonomura <nekonomuranomirai@gmail.com>
2024-11-18 22:09:34 +0900

    all

---
+++ src/main/java/net/nekonomura/allthematerialmanagement/allthematerialmanagement.java
@@ -0,0 +1,33 @@
+package net.nekonomura.allthematerialmanagement;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+//f
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("allthematerialmanagement")
+public class allthematerialmanagement
+{
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public allthematerialmanagement()
+    {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
